fa5e4026e54ea3641ad8b320b4de31fd27b5d5c5
LarisaZaripova <larik7lolik@gmail.com>
2022-03-10 14:18:27 +0300

    added Example 65

---
+++ Example_65/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 65: Спирально заполнить двумерный массив:
+/* 1  2  3  4
+12 13 14 5
+11 16 15 6
+10  9  8 7 */
+
+void Main()
+{
+    int[,] array = new int[5, 5];
+    SnakeFill(array.GetLength(0), array.GetLength(1), array);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int y = 0; y < array.GetLength(0); y++)
+    {
+        for (int x = 0; x < array.GetLength(1); x++)
+            Console.Write($"{array[y, x]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void SnakeFill(int endRow, int endCol, int[,] array)
+{
+    int value = 1;
+    int startRow = 0;
+    int startCol = 0;
+
+    while (startRow < endRow && startCol < endCol)
+    {
+        for (int i = startCol; i < endCol; ++i)
+            array[startRow, i] = value++;
+
+        startRow++;
+
+        for (int i = startRow; i < endRow; ++i)
+            array[i, endCol - 1] = value++;
+
+        endCol--;
+
+        if (startRow < endRow)
+        {
+            for (int i = endCol - 1; i >= startCol; --i)
+                array[endRow - 1, i] = value++;
+        }
+
+        endRow--;
+
+        if (startCol < endCol)
+        {
+            for (int i = endRow - 1; i >= startRow; --i)
+                array[i, startCol] = value++;
+        }
+
+        startCol++;
+    }
+    PrintArray(array);
+}
+Main();
