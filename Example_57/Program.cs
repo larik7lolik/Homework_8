124e7827dbf0b73070040108868f1b8cf815e812
LarisaZaripova <larik7lolik@gmail.com>
2022-03-10 13:05:43 +0300

    added Example 57

---
+++ Example_57/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 57: Написать программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.
+Console.Clear();
+int[,] array = new int[4, 6];
+
+void FillArray()
+{
+    Random number = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = number.Next(1, 100);
+        }
+    }
+}
+
+void Printarray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+void SortNumbers()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = array.GetLength(1) - 1; j > 0; j--)
+        {
+            for (int k = 0; k < j; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+FillArray();
+Printarray(array);
+SortNumbers();
+Console.WriteLine();
+Printarray(array);
+
